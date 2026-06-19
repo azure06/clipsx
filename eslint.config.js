79f95d8237655b9ad3656331e568a444938e617b
Gabriele Sato <gabri06e@gmail.com>
2026-06-19 23:23:09 +0900

    fix: suppress unused variable lint warning with varsIgnorePattern

--- eslint.config.js
+++ eslint.config.js
@@ -42,7 +42,7 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       // TypeScript recommended overrides
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   }
